8da21e4920f92f90b2723db5e6a8d0774914d6e3
pandhu <pandhu.hutomo@ui.ac.id>
2015-09-09 15:04:56 +0700

    first unit test and url mappin, dummy view

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,12 +1,8 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 
-from django.contrib import admin
-admin.autodiscover()
+from lists import views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'superlists.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^$', views.home_page, name='home'),
+    # url(r'^admin/', include(admin.site.urls)),
+]
